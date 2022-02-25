dbe00751ea0187a06decbd68bcfab00abf77eda1
devchoplife <dritedurojaiye@gmail.com>
2022-02-25 20:49:51 +0100

    Add data types file

---
+++ 5 - data-types.py
@@ -0,0 +1,46 @@
+# Data types 
+"""
+Text type - str
+numeric types - int, float, complex
+sequence types - list, tuple, range
+mapping type - dict 
+set types - set, frozenset
+boolean type - bool
+binary types - bytes, bytearray, memoryview
+"""
+
+# Setting the Data Types 
+"""
+x = "Hello World" => str	
+x = 20 => int	
+x = 20.5 => float	
+x = 1j => complex	
+x = ["apple", "banana", "cherry"] => list	
+x = ("apple", "banana", "cherry") => tuple	
+x = range(6) => range	
+x = {"name" : "John", "age" : 36} => dict	
+x = {"apple", "banana", "cherry"} => set	
+x = frozenset({"apple", "banana", "cherry"}) => frozenset	
+x = True => bool	
+x = b"Hello" => bytes	
+x = bytearray(5) => bytearray	
+x = memoryview(bytes(5)) => memoryview
+"""
+
+# Setting the specific data types 
+"""
+x = str("Hello World") => str	
+x = int(20) => int	
+x = float(20.5) => float	
+x = complex(1j) => complex	
+x = list(("apple", "banana", "cherry")) => list	
+x = tuple(("apple", "banana", "cherry")) => tuple	
+x = range(6) => range	
+x = dict(name="John", age=36) => dict	
+x = set(("apple", "banana", "cherry")) => set	
+x = frozenset(("apple", "banana", "cherry")) => frozenset	
+x = bool(5) => bool	
+x = bytes(5) => bytes	
+x = bytearray(5) => bytearray	
+x = memoryview(bytes(5)) => memoryview
+"""
