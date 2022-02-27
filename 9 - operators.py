0b028537afc6cdd633c923d557c50dccedc578fe
devchoplife <dritedurojaiye@gmail.com>
2022-02-28 00:10:58 +0100

    Operators

---
+++ 9 - operators.py
@@ -0,0 +1,81 @@
+# Arithmetic operators
+# Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+"""
++ +>A ddition
+- +> Subtraction
+* +> Multiplication
+/ +> Division
+% +> Modulus
+** +> Exponentiation	 
+// +> Floor division
+"""
+
+# Assignment operators 
+# Assignment operators are used to assign values to vars 
+
+"""
+= => x = 5
++= => x += 3    
+-= => x -= 3    
+*= => x *= 3    
+/= => x /= 3    
+%= => x %= 3    
+//= => x //= 3     
+**= => x **= 3	    
+&= => x &= 3    
+|= => x |= 3    
+^= => x ^= 3    
+>>= => x >>= 3     
+<<= => x <<= 3    
+"""
+
+# Comparison operators 
+#Comparison operators are used to compare two values:
+
+"""
+== => Equal	
+!= => Not equal	
+> => Greater than
+< => Less than
+>= => Greater than or equal to	
+<= => Less than or equal to
+"""
+
+# Logical operators 
+# Logical operators are used to combine conditional statements 
+
+"""
+and  = Returns True if both statements are true
+or = Returns True if one of the statements is true
+not = Reverse the result, returns False if the result is true
+"""
+
+# Identity operators
+# Identity operators are used to compare the objects, not if they are equal, 
+# but if they are actually the same object, with the same memory location:
+
+"""
+is => Returns True if both variables are the same object
+is not => Returns True if both variables are not the same object
+"""
+
+# Membership operators 
+# Membership operators are used to test if a sequence is presented in an object:
+
+"""
+in => Returns True if a sequence with the specified value is present in the object
+not in => Returns True if a sequence with the specified value is not present in the object
+"""
+
+# Bitwise operators
+# Bitwise operators are used to compare binary numbers 
+
+"""
+&  => AND => Sets each bit to 1 if both bits are 1
+| => OR => Sets each bit to 1 if one of two bits is 1
+ ^ => XOR => Sets each bit to 1 if only one of two bits is 1
+~  => NOT => Inverts all the bits
+<< => Zero fill left shift => Shift left by pushing zeros in from the right and let the leftmost bits fall off
+>> => Signed right shift => Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+"""
