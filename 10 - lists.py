c53012ea3a1501ea1cfcecc61751df418672d3d7
devchoplife <dritedurojaiye@gmail.com>
2022-02-28 19:04:12 +0100

    List comprehension

--- 10 - lists.py
+++ 10 - lists.py
@@ -119,3 +119,32 @@ while i < len(thislist):
 # list comprehension offers the shortest syntax for looping through lists
 thislist = ["apple", "banana", "cherry"]
 [print(x) for x in thislist]
+
+
+# LIST COMPREHENSION
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+    if "a" in x:
+        newlist.append(x)
+
+print(newlist)
+# can be simplified with list comprehension to:
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
+
+# List comprehension syntax
+# The condition is like a filter that only accepts the items that valuate to True.
+# newlist = [expression for item in iterable if condition == True]
+# the iterable can be any iterable object like a list, tuple, set etc.
+# You can also use range to create an iterable
+newlist = [x for x in range(10)]
+"""
+The expression is the current item in the iteration, but it is also the outcome, 
+which you can manipulate before it ends up like a list item in the new list:
+"""
+newlist = [x.upper() for x in fruits]
+# The expression can also contain conditions, not like a filter, but as a way to manipulate the outcome:
+newlist = [x if x != "banana" else "orange" for x in fruits]
