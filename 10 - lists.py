671efd019e72bb8ee21a5e598bcc47090ef7e53b
devchoplife <dritedurojaiye@gmail.com>
2022-02-28 18:43:56 +0100

    Looping through lists

--- 10 - lists.py
+++ 10 - lists.py
@@ -96,3 +96,26 @@ del thislist
 thislist = ["apple", "banana", "cherry"]
 thislist.clear()
 print(thislist)
+
+# LOOP LISTS
+# Loop through a list
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+    print(x)
+
+# Loop through the index numbers
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+    print(thislist[i])
+
+# Using a while loop
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+    print(thislist[i])
+    i = i + 1
+
+# Looping using a list comprehension
+# list comprehension offers the shortest syntax for looping through lists
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
