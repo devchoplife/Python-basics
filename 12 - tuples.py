6e8ef60aa0972d85aa85385e6432b02143feb551
devchoplife <dritedurojaiye@gmail.com>
2022-03-03 09:21:06 +0100

    Pack and unpack tuples

--- 12 - tuples.py
+++ 12 - tuples.py
@@ -89,3 +89,42 @@ thistuple = tuple(y)
 thistuple = ("apple", "banana", "cherry")
 del thistuple
 print(thistuple)  # this will raise an error because the tuple no longer exists
+
+# UNPACK TUPLES
+# Unpacking a tuple
+# WHen we creat e tuple, we normally assign a value to it. This is called "packing" a tuple.
+fruits = ("apple", "banana", "cherry")  # packing
+
+# We are also allowec the extract thevalues back ito vars. Called Unpacking
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits  # vars to unpack into
+
+print(green)
+print(yellow)
+print(red)
+
+# Using an asterisk (*)
+"""
+If the number of variables is less than the number of values, you can add an * to the 
+variable name and the values will be assigned to the variable as a list:
+"""
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+"""
+If the asterisk is added to another variable name than the last, Python will assign values 
+to the variable until the number of values left matches the number of variables left.
+"""
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
