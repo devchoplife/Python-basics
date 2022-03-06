d0166edcc8a51e704bc8c68b8d256b4f8659c3c3
devchoplife <dritedurojaiye@gmail.com>
2022-03-06 22:55:03 +0100

    Remove set items

--- 13 - sets.py
+++ 13 - sets.py
@@ -53,3 +53,40 @@ thisset = {"apple", "banana", "cherry"}
 mylist = ["kiwi", "orange"]
 thisset.update(mylist)
 print(thisset)
+
+# Remove set items
+"""To remove an item in a set, use the remove(), or the discard() method."""
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+# or
+
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+
+# the difference between remove and discard is that if the item to remove does not exist
+# remove will raise an error while discard will not raise an error
+
+"""
+You can also use the pop() method to remove an item, but this method will remove the 
+last item. Remember that sets are unordered, so you will not know what item that gets 
+removed.
+
+The return value of the pop() method is the removed item.
+"""
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x)
+print(thisset)
+
+"""The clear() method empties the set"""
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+
+"""The del keyword will delete the set completely:"""
+thisset = {"apple", "banana", "cherry"}
+del thisset
+print(thisset)
