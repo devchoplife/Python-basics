6fc105c5cfe918408493686affa4e14158bef644
devchoplife <dritedurojaiye@gmail.com>
2022-03-06 22:45:26 +0100

    Access set items

--- 13 - sets.py
+++ 13 - sets.py
@@ -15,3 +15,19 @@ set1 = {"abc", 34, True, 40, "male"}
 # The set constructor
 thisset = set(("apple", "banana", "cherry"))  # note the double round-brackets
 print(thisset)
+
+# Access set items
+"""
+You cannot access items in a set by referring to an index or a key.
+
+But you can loop through the set items using a for loop, or ask if a specified 
+value is present in a set, by using the in keyword
+"""
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+    print(x)
+
+# or
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
