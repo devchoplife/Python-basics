d0714b5790a34de278f4226770e42f4aea0e4c8f
devchoplife <dritedurojaiye@gmail.com>
2022-03-06 22:49:33 +0100

    Add set items

--- 13 - sets.py
+++ 13 - sets.py
@@ -31,3 +31,25 @@ for x in thisset:
 # or
 thisset = {"apple", "banana", "cherry"}
 print("banana" in thisset)
+
+# Add set items
+"""To add one item to a set use the add() method."""
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+"""To add items from another set into the current set, use the update() method."""
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
+
+# Add any iterable
+"""
+The object in the update() method does not have to be a set, it can be any iterable 
+object (tuples, lists, dictionaries etc.)
+"""
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
