14225951dd8105f1a9f3a40c04f7ee88d6935b3e
devchoplife <dritedurojaiye@gmail.com>
2022-03-06 22:44:28 +0100

    Sets

---
+++ 13 - sets.py
@@ -0,0 +1,17 @@
+"""
+Sets have the following attributes:
+- unordered 
+- unchangeable 
+- unindexed 
+- do not allow duplicate values 
+"""
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+# len() can be used to get the length of a set
+# Set items can be of any data type
+set1 = {"abc", 34, True, 40, "male"}
+
+# The set constructor
+thisset = set(("apple", "banana", "cherry"))  # note the double round-brackets
+print(thisset)
