3a799f657d6901fd5368aa8a198dcadfceec2045
devchoplife <dritedurojaiye@gmail.com>
2022-03-07 11:35:51 +0100

    Add Dictionary items

--- 14 - dictionaries.py
+++ 14 - dictionaries.py
@@ -69,3 +69,23 @@ thisdict = {
     "year": 1964
 }
 thisdict.update({"year": 2020})
+
+# Add dictionary items
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+"""The update() method will update the dictionary with the items from a given argument. 
+If the item does not exist, the item will be added.
+
+The argument must be a dictionary, or an iterable object with key:value pairs."""
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.update({"color": "red"})
