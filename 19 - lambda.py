f3313b1710e94b6359d3d0b8dc0a8118588f78a6
devchoplife <dritedurojaiye@gmail.com>
2022-03-07 18:07:16 +0100

    Python lambda

---
+++ 19 - lambda.py
@@ -0,0 +1,21 @@
+# Lambda function
+"""A lambda function is a small anonymous function.
+A lambda function can take any number of arguments, but can only have one expression"""
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+# Why use lambda functions 
+"""The power of lambda is better shown when you use them as an anonymous function 
+inside another function.
+
+Say you have a function definition that takes one argument, and that argument will 
+be multiplied with an unknown number"""
+def myfunc(n):
+    return lambda a : a * n
+
+mydoubler = myfunc(2)
+print(mydoubler(11))
+
