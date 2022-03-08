e8fd7337fed43dda4664049ceabe4bad03308de0
devchoplife <dritedurojaiye@gmail.com>
2022-03-08 15:27:31 +0100

    Math

---
+++ 25 - math.py
@@ -0,0 +1,30 @@
+# min and max
+import math
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# get absolute value
+x = pow(4, 3)
+
+print(x)
+
+# sqrt
+x = math.sqrt(64)
+
+print(x)
+
+"""The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds 
+a number downwards to its nearest integer, and returns the result"""
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)  # returns 2
+print(y)  # returns 1
+
+# pi
+x = math.pi
+
+print(x)
